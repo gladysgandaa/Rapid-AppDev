823865e5acf3f14b920ae6fab7d59fbaf7e83e45
Gladys Ganda <s3679839@student.rmit.edu.au>
2020-03-25 09:04:19 +0000

    Initialize Repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get 'hello' => 'hello#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
